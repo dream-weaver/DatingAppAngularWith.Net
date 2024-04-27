f7b09550c89757d3a5d2efc2e4fe2bb1e84db38d
Tanvir Ahmed <eye_tanvir@hotmail.com>
2024-04-27 20:08:14 +0600

    Initial commit

---
+++ DatingAPI/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using DatingAPI.Data;
+using DatingAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPI.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+	{
+		private readonly DataContext _dbContext;
+		public UsersController(DataContext dataContext)
+		{
+			_dbContext = dataContext;
+		}
+		//api/users
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+		{
+			return await _dbContext.Users.ToListAsync();
+		}
+		//api/users/1
+		[HttpGet("{id}")]
+		public async Task<ActionResult<AppUser>> GetUsersById(int id)
+		{
+			return await _dbContext.Users.FindAsync(id);
+		}
+	}
+}
